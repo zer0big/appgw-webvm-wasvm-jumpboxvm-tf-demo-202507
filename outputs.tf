59e71e566d52563d48b457ec0f24042968f469a2
zerobig <zerobig.kim@gmail.com>
2025-07-16 13:09:06 +0900

    Update output messagess to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 # outputs.tf
 
 output "web_vm_private_ip" {
-  description = "Web VM의 사설 IP 주소"
+  description = "Web VM Private IP Address"
   value       = azurerm_network_interface.web_nic.private_ip_address
 }
 
 output "app_db_vm_private_ip" {
-  description = "App/DB VM의 사설 IP 주소"
+  description = "App/DB VM Private IP Address"
   value       = azurerm_network_interface.app_nic.private_ip_address
 }
 
 output "jumpbox_vm_public_ip" {
-  description = "Jumpbox VM의 공용 IP 주소"
+  description = "Jumpbox VM Public IP Address"
   value       = azurerm_public_ip.jumpbox_public_ip.ip_address
 }
 
 output "application_gateway_public_ip" {
-  description = "Application Gateway의 공용 IP 주소"
+  description = "Application Gateway Public IP Address"
   value       = azurerm_public_ip.appgw_public_ip.ip_address
 }
